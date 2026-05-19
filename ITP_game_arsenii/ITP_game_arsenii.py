a5db6e214ca693d22de6ed49375df6a0636e0023
ars02280 <spakspak01@gmail.com>
2026-05-19 21:00:02 +0500

    Initial commit

---
+++ ITP_game_arsenii/ITP_game_arsenii.py
@@ -0,0 +1,16 @@
+import pygame;
+import sys;
+
+pygame.init();
+WIDTH, HEIGHT = 1600, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT));
+print("wgt");
+
+
+running = True
+
+
+
+
+while running:
+    pass;
