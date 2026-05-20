7916090fa8c1f69fb2ab681576932cc214ba84e2
ars02280 <spakspak01@gmail.com>
2026-05-20 21:52:00 +0500

    base and events added

--- ITP_game_arsenii/ITP_game_arsenii.py
+++ ITP_game_arsenii/ITP_game_arsenii.py
@@ -1,9 +1,12 @@
-import pygame;
-import sys;
-import time;
+import pygame
+import sys
+import json
+import os
 
 pygame.init();
 WIDTH, HEIGHT = 1600, 900
+DATA_FILE = "highscores.json"
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT));
 clock = pygame.time.Clock()
 
@@ -15,11 +18,72 @@ start_btn = pygame.Rect(300, 220, 200, 50)
 exit_btn = pygame.Rect(300, 300, 200, 50)
 
 WHITE = (255, 255, 255)
-DARK_BG = (30, 30, 40)
-GAME_BG = (50, 100, 50)
+BLACK = (0, 0, 0)
+DARK_BG = (20, 20, 30)
+GAME_BG = (35, 35, 45)
 BUTTON_COLOR = (70, 130, 180)
 BUTTON_HOVER = (100, 149, 237)
 
+
+
+class Game:
+    def __init__(self):
+  
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("ITP Game")
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+    
+        self.state = "MENU"
+        
+   
+        self.font_title = pygame.font.SysFont("Arial", 50)
+        self.font_ui = pygame.font.SysFont("Arial", 30)
+        
+        self.start_btn = pygame.Rect(300, 250, 200, 50)
+        self.exit_btn = pygame.Rect(300, 330, 200, 50)
+        
+   
+        self.score = 0
+        #self.high_score = self.load_high_score() 
+
+
+
+
+        def handle_events(self):
+       
+            mouse_pos = pygame.mouse.get_pos()
+        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                
+                
+                if self.state == "MENU":
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        if self.start_btn.collidepoint(mouse_pos):
+                            self.reset_game()
+                            self.state = "GAME"
+                        elif self.exit_btn.collidepoint(mouse_pos):
+                            self.running = False
+                        
+                
+                elif self.state == "GAME":
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            self.check_and_update_highscore()
+                            self.state = "MENU"
+                        
+                        if event.key == pygame.K_k:
+                            self.check_and_update_highscore()
+                            self.state = "GAMEOVER"
+
+            
+                elif self.state == "GAMEOVER":
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE or event.key == pygame.K_ESCAPE:
+                            self.state = "MENU"
 
 
 
@@ -36,10 +100,8 @@ def draw_game():
 
 
 
-while running:
-    if state == "MENU":
-                draw_menu()
-    if state == "GAME":
-        draw_game;
-    pygame.display.flip();
-    clock.tick(60);
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pygame.quit()
+    sys.exit()
